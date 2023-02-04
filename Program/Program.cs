d521a2a8590021e002b6640799d24a87aab10c76
Vkapeschin <kapeshin-vladimir@rambler.ru>
2023-02-04 21:27:48 +0300

    methods (FillStringArray, ShowArray) added

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FillStringArray()
+{
+    Console.Write("Введите количество элементов массива: ");
+    int stringLength = Convert.ToInt32(Console.ReadLine());
+
+    string[] stringArray = new string[stringLength];
+
+    for (int i = 0; i < stringLength; i++)
+    {
+        Console.Write($"Введите {i+1}-й элемент массива: ");
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
+void ShowArray(string[] strArray)
+{
+    Console.WriteLine("Массив состоит из следующих элементов: ");
+    for (int i = 0; i < strArray.Length; i++)
+        Console.WriteLine($"{i+1}. {strArray[i]};");
+}
+
+string[] strArray = FillStringArray();
+ShowArray(strArray);
